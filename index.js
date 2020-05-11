a58c5ccdcb30e2bc9a4b91d43fc92811e05d5a12
Chazensign <chazensign@gmail.com>
2020-05-11 13:35:54 -0600

    additional federal withholdings displaying and removing properly

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function numHandleChange(e) {
   let inputVal = document.getElementById(e.target.name)
-  var regex = /^([1-9]\d*|0)(\.\d+)?$/g
+  let regex = /^([1-9]\d*|0)(\.\d+)?$/g
   if (!e.target.value.match(regex)) {
     inputVal.setCustomValidity(
       `Invalid input for ${
@@ -44,6 +44,123 @@ async function stateSelected(e) {
   //   })
   //   .catch((error) => console.log('error', error))
 }
+let count = 1
+let onlyCheckbox
+
+function addWithholding(e) {
+  let isChecked = document.getElementById('fed-with-check').checked
+
+  if (count === 1) {
+    onlyCheckbox = document.getElementById('fed-withs-cont').innerHTML
+  }
+  e.preventDefault()
+
+  const container = document.getElementById('fed-withs-cont')
+  const newForm = `<div \
+      id='fed-with-form-${count}' \
+      class='fed-with-form fed-${count}' \
+      style='margin-top: 5px;'> \
+      <h3 style='margin-top: 5px;'>Additional Federal Withholding ${count}</h3> \
+      <div class='form-line'> \
+        <label for='add-fed-name'>Withholding Name</label> \
+        <div> \
+          <input \
+            form='fed-form' \
+            required \
+            type='text' \
+            id='add-fed-name-${count}' \
+          /> \
+        </div> \
+      </div> \
+      <div class='form-line'> \
+        <label for='add-fed'>Withholding Amount</label> \
+        <div class='dollar'> \
+          <input \
+            form='fed-form' \
+            required \
+            name='fed-with-amount-${count}' \
+            type='text' \
+            id='fed-with-amount-${count}' \
+            onkeyup='numHandleChange(event)' \
+          /> \
+        </div> \
+      </div> \
+      <div class="form-line"> \
+          <label for="fed-method">Amount Frequency</label> \
+          <select \
+          form="fed-form" \
+            name="pay-method" \
+            id="fed-method" \
+            style="overflow: hidden !important; width: 150px;" \
+          > \
+            <option value="annual">Annually</option> \
+            <option value="perPeriod">Pay Per Period</option> \
+          </select> \
+        </div> \
+      <div class='form-line checkbox'> \
+        <label class='info' for='pre-tax-fed'> \
+          Pre-Tax Withholding \
+        </label> \
+        <input form='fed-form' type='checkbox' id='pre-tax-fed-${count}'/> \
+        <label class='check' for='pre-tax-fed-${count}'></label> \
+      </div> \
+      
+      <button \
+        id='fed-button-${count}'
+        class='fed-button' \
+        form='fed-form' \
+        style='align-self: center; display: ${
+          count === count ? 'block' : 'none'
+        }' \
+        onclick='addWithholding(event)'> \
+        Add \
+      </button> \
+      <button 
+      style='display:${count === 1 ? 'none' : 'block'}'
+      onclick="removeWithholding(event)"
+      >
+      Remove
+      </button>
+    </div>`
+  if (isChecked) {
+    if (count > 1) {
+      document.getElementById(`fed-button-${count - 1}`).style =
+        'align-self: center; display: none'
+    }
+    container.insertAdjacentHTML('beforeend', newForm)
+    count++
+  } else {
+    container.innerHTML = onlyCheckbox
+    count = 1
+  }
+}
+
+function removeWithholding(e) {
+  e.preventDefault()
+  document.getElementById(e.target.parentNode.id).remove()
+  count--
+}
+
+function getWithholdings() {
+  const fedWithsArr = document.getElementsByClassName('fed-with-form')
+  let preTax = []
+  let postTax = []
+  for (let i = 0; i < fedWithsArr.length; i++) {
+    
+    console.log(fedWithsArr[i].querySelectorAll('input, select'))
+    let inputs = fedWithsArr[i].querySelectorAll('input, select')
+    return {
+      name: inputs[0].value,
+      amount: inputs[1].value,
+      preTax: inputs[2].value,
+      frequency: inputs[3].value
+    }
+  }
+  return {
+    preTax,
+    postTax,
+  }
+}
 
 function displayRes(resObj) {
   Object.keys(resObj).forEach((loc) => {
@@ -55,26 +172,31 @@ function displayRes(resObj) {
   const results = document.getElementById('results')
   const inputs = document.getElementById('main-container')
   inputs.style = 'display: none'
-  results.style = 'display: block'
+  results.style = 'display: flex'
 }
 
 async function onSubmit(e) {
   e.preventDefault()
+  const withholdings = getWithholdings()
+  console.log(withholdings);
+  
   let socialSec = 0
   let medicare = 0
   let payFrequency = document.getElementById('freq').value
   let grossInput = +document.getElementById('pay').value
   let method = document.getElementById('method').value
-  let exemptions = document.getElementById('fed-allow').value
+  // let exemptions = document.getElementById('fed-allow').value
   const fedExempt = document.getElementById('ex-fed').checked
   const socExempt = document.getElementById('ex-soc').checked
   const medExempt = document.getElementById('ex-med').checked
   const stateExempt = document.getElementById('ex-state').checked
+  const addFedHold = document.getElementById('add-fed').value
+
   if (!payFrequency || !grossInput || !method || !state) {
     return alert('Please fill out required fields.')
   }
   if (method === 'perPeriod') {
-    grossInput *= payFrequency 
+    grossInput *= payFrequency
   }
   if (grossInput > 137700) {
     socialSec = 8537.4
@@ -121,12 +243,13 @@ async function onSubmit(e) {
       res.annual.state.amount = stateExempt ? 0 : res.annual.state.amount
       res.annual.social = { amount: socExempt ? 0 : socialSec }
       res.annual.medicare = { amount: medExempt ? 0 : medicare }
-      res.annual.fica.amount = res.annual.social.amount + res.annual.medicare.amount
+      res.annual.fica.amount =
+        res.annual.social.amount + res.annual.medicare.amount
       for (const key in res.annual) {
         if (key === 'fica') continue
         else annualDeductions += res.annual[key].amount
       }
-      res.annual.gross = {amount: grossInput}
+      res.annual.gross = { amount: grossInput }
       res.annual.net = { amount: grossInput - annualDeductions }
       let periodDeductions = 0
       res.per_pay_period.social = {
@@ -135,8 +258,12 @@ async function onSubmit(e) {
       res.per_pay_period.medicare = {
         amount: medExempt ? 0 : medicare / payFrequency,
       }
-      res.per_pay_period.state.amount = stateExempt ? 0 : res.per_pay_period.state.amount
-      res.per_pay_period.federal.amount = fedExempt ? 0 : res.per_pay_period.federal.amount
+      res.per_pay_period.state.amount = stateExempt
+        ? 0
+        : res.per_pay_period.state.amount
+      res.per_pay_period.federal.amount = fedExempt
+        ? 0
+        : res.per_pay_period.federal.amount
       for (const key in res.per_pay_period) {
         if (key === 'fica') continue
         else periodDeductions += res.per_pay_period[key].amount
@@ -152,9 +279,9 @@ async function onSubmit(e) {
     .catch((error) => console.log('error', error))
 }
 function goBack(e) {
-e.preventDefault()
-const inputs = document.getElementById('main-container')
-const results = document.getElementById('results')
-results.style = 'display: none' 
-inputs.style = 'display: block'
+  e.preventDefault()
+  const inputs = document.getElementById('main-container')
+  const results = document.getElementById('results')
+  results.style = 'display: none'
+  inputs.style = 'display: block'
 }
